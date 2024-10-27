2ea10f6dab1e7f56fb8f3cac8e224e572a589457
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-27 23:43:52 +0100

    Initial commit with READEME and task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This script defines as function `index_range`:
+The function returns a tuple of start and end index
+"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    returns a two sized tuple with the
+    start and end of the index range
+    '''
+    return (page - 1) * page_size, page_size * page
