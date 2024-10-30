185c30526fdbaf8cf57ddc76ca7fa1490009dc11
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-30 23:56:01 +0100

    added task 3: Least recent used

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""This script implements a basic cache class"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    A basic caching system class. The LIFO implementation.
+    cache must not be more than 4 items
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """update the cache with given key and item"""
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                    lr_key, _ = self.cache_data.popitem(False)
+                    print(f'DISCARD: {lr_key}')
+            self.cache_data.update({key: item})
+            self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """
+        return value of given key.
+        if key is None or doesn't exist return None
+        """
+        if key and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data.get(key)
