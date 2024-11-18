ffa5034ddda13ae51d29880b220b26333eb15897
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-18 23:05:29 +0100

    correction pycodestyle compliance

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,14 @@
 """A simple flask app"""
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
+    """index route rendition"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,19 +4,22 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel()
-
 class Config:
     """a class to configure available languages"""
     LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+babel = Babel()
 app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
 app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
+    """index route rendition"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
